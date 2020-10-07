715e1715260be4318916c184bd12ad5cb34dee55
phungngochuyhoang <phungngochuyhoang@gmail.com>
2020-10-07 21:50:41 +0700

    first commit

---
+++ Resources/Js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    $('.close-user').click(() => {
+        $('.bx-user').fadeOut(100)
+    })
+    $('.img-circle').click(() => {
+        $('.bx-user').fadeIn(400)
+    })
+
+    $('.bar-nav').click(() => {
+        $('.bar-nav').hide();    
+        $('.ul-mobile').animate({'left': 0 }, 1000)
+    })
+
+    $('.close-nav').click(() => { 
+        $('.ul-mobile').animate({'left': -100 + "%"}, 1000)
+        setInterval(() => {
+            $('.bar-nav').show();  
+        }, 1000) 
+    })
+})
+
+
