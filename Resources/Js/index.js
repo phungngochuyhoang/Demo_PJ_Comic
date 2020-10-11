5eb9142b3f9d4b98fc5d1d29935bebc08776064b
phungngochuyhoang <phungngochuyhoang@gmail.com>
2020-10-11 21:31:47 +0700

    add code

--- Resources/Js/index.js
+++ Resources/Js/index.js
@@ -1,3 +1,4 @@
+// bar navigation
 $(document).ready(() => {
     $('.close-user').click(() => {
         $('.bx-user').fadeOut(100)
@@ -17,5 +18,29 @@ $(document).ready(() => {
     })
 })
 
+// slide hot story
 
+var obj = {
+    slidesPerView: 6,
+    spaceBetween: 10,
+    freeMode: true,
+    loop: true,
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+}
 
+if(window.innerWidth <= 1024 && window.innerWidth > 568) {
+   obj.slidesPerView = 5;
+}else if(window.innerWidth <= 568 && window.innerWidth >= 360) {
+    obj.slidesPerView = 4;
+}
+
+var swiper1 = new Swiper('.slide-hot', obj);
+
+console.log(window.innerWidth);
