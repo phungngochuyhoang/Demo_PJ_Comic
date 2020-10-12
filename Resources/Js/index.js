65996fdd1db0d96919fcbc509f805d26628bf5ee
phungngochuyhoang <phungngochuyhoang@gmail.com>
2020-10-12 23:15:42 +0700

    add code

--- Resources/Js/index.js
+++ Resources/Js/index.js
@@ -6,7 +6,7 @@ $(document).ready(() => {
     $('.img-circle').click(() => {
         $('.bx-user').fadeIn(400)
     })
-
+    // -----------------------------------------
     $('.bar-nav').click(() => {
         $('body').css({'overflow': 'hidden'})
         $('.bar-nav').hide();    
@@ -18,13 +18,13 @@ $(document).ready(() => {
         $('.ul-mobile').animate({'left': -100 + "%"}, 1000)
         $('.bar-nav').show();  
     })
+    // --------------------------------------------
 })
 
 // slide hot story
-
 var objSlideHot = {
     slidesPerView: 6,
-    spaceBetween: 10,
+    spaceBetween: 8,
     freeMode: true,
     loop: true,
     pagination: {
@@ -37,8 +37,14 @@ var objSlideHot = {
     },
 }
 
-if(window.innerWidth <= 768 && window.innerWidth >360) {
+if(window.innerWidth <= 768 && window.innerWidth >= 360) {
     objSlideHot.slidesPerView = 1;
 }
 
-var swiperHot = new Swiper('.slide-hot', objSlideHot);
+function swiper(object) {
+   return new Swiper('.slide-hot', object);
+}
+
+swiper(objSlideHot);
+
+
